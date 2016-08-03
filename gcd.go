3854880f5d4016633de7cd8096bed549332c87a5
Trần Duy Thanh <coachtranduthanh@gmail.com>
2016-08-03 13:50:24 +0700

    fix wrong function name

--- gcd.go
+++ gcd.go
@@ -1,12 +1,12 @@
 package gomath
 
 // GCDint64 returns Greatest Common Divisor of 2 int64
-func GCD(a, b int64) int64 {
+func GCDint64(a, b int64) int64 {
 	c := a % b
 
 	if c == 0 {
 		return b
 	}
 
-	return GCD(b, c)
+	return GCDint64(b, c)
 }
